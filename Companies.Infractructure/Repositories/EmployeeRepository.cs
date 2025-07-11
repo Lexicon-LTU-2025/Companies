579a7b5fa5a6c215450db724f855e9d128c20214
DZM77 <dimitris.bjorlingh@outlook.com>
2025-07-11 10:57:58 +0200

    EmployeeRepository

---
+++ Companies.Infractructure/Repositories/EmployeeRepository.cs
@@ -0,0 +1,20 @@
+﻿using Companies.Infractructure.Data;
+using Domain.Contracts.Repositories;
+using Domain.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Companies.Infractructure.Repositories;
+public class EmployeeRepository : RepositoryBase<Employee>, IEmployeeRepository
+{
+    public EmployeeRepository(ApplicationDbContext context) : base(context) { }
+
+    public async Task<IEnumerable<Employee>> GetEmployeesAsync(Guid companyId, bool trackChanges = false)
+    {
+        return await FindByCondition(e => e.CompanyId == companyId, trackChanges).ToListAsync();
+    }
+}
---
+++ Domain.Contracts/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,7 @@
+﻿using Domain.Models.Entities;
+
+namespace Domain.Contracts.Repositories;
+public interface IEmployeeRepository
+{
+    Task<IEnumerable<Employee>> GetEmployeesAsync(Guid companyId, bool trackChanges = false);
+}
