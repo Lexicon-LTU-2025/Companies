5d95110003477085601cdc44380dc7483a30a2b1
DZM77 <dimitris.bjorlingh@outlook.com>
2025-07-14 12:04:48 +0200

    To Extension

---
+++ Companies.API/Extensions/ExceptionMiddlewareExtetensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Companies.API.Extensions;
+
+public static class ExceptionMiddlewareExtetensions
+{
+    public static void ConfigureExceptionHandler(this WebApplication app)
+    {
+        app.UseExceptionHandler(builder =>
+        {
+            builder.Run(async context =>
+            {
+                var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                if (contextFeature != null)
+                {
+                    var problemDetails = new ProblemDetails
+                    {
+                        Status = context.Response.StatusCode,
+                        Title = "Internal Server Error",
+                        Detail = contextFeature.Error.Message,
+                        Instance = context.Request.Path,
+                        Type = "https://httpstatuses.com/500"
+                    };
+
+                    context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                    await context.Response.WriteAsJsonAsync(problemDetails);
+                }
+            });
+        });
+    }
+}
--- Companies.API/Program.cs
+++ Companies.API/Program.cs
@@ -36,27 +36,7 @@ namespace Companies.API
 
 
 
-            app.UseExceptionHandler(builder =>
-            {
-                builder.Run(async context =>
-                {
-                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
-                    if(contextFeature != null)
-                    {
-                        var problemDetails = new ProblemDetails
-                        {
-                            Status = context.Response.StatusCode,
-                            Title = "Internal Server Error",
-                            Detail = contextFeature.Error.Message,
-                            Instance = context.Request.Path,
-                            Type = "https://httpstatuses.com/500"
-                        };
-
-                        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
-                        await context.Response.WriteAsJsonAsync(problemDetails);
-                    }
-                });
-            });
+            app.ConfigureExceptionHandler();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
